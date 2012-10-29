f613bb4bad1098f7f06781f1bf043247dbf10118
たなかともあき <github@bamboo-copter.com>
2012-10-29 11:21:08 +0900

    もうvspでやるからいいもん

--- lib/sc_socket.c
+++ lib/sc_socket.c
@@ -1,6 +1,11 @@
+#include <stdio.h>
 #include <string.h>
+#include <unistd.h>
+
+#include <arpa/inet.h>
 #include <netinet/in.h>
 #include <sys/socket.h>
+
 #include "sc_socket.h"
 
 int serverSocket(char *host, int port)
